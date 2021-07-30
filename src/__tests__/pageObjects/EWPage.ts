6268f08f32512e5f6a7faa91c5e5edef9c1c5f41
Uzoma Nwokolo <uzo.nwokolo@gmail.com>
2021-07-30 18:03:28 -0500

    Added more locators to EWPage, added methods as well

--- src/__tests__/pageObjects/EWPage.ts
+++ src/__tests__/pageObjects/EWPage.ts
@@ -33,5 +33,55 @@ export class EWPage {
     clearBtn: By = By.id('clearBtn');
     /** Error message list */
     errorMsgList: By = By.id('errorList');
-    
+    /** Valid query */
+    validTitle: By = By.id('validHeader');
+    result: By = By.name('queryBody');
+
+
+    /**
+     * This is the constructor for the SpecPage class
+     * @param drive - The WebDriver object
+     */
+     constructor(drive: WebDriver) {
+        this.driver = drive;
+    }
+
+    /**
+     * This navigates to the homepage specified in the url string
+     */
+     async openApp() {
+        await this.driver.get(this.appUrl);
+        await this.driver.wait(until.elementLocated(this.appTitle));
+        await this.driver.wait(
+            until.elementIsVisible(await this.driver.findElement(this.appTitle))
+        );
+    }
+
+    /** 
+     * Fills input to the page element
+     * @param locator - the input element
+     * @param keys - the string to input
+     */
+     async sendKeys(locator: By, keys: string) {
+        await this.driver.wait(until.elementLocated(locator));
+        return this.driver.findElement(locator).sendKeys(keys);
+    }
+
+    /** Click action for page elements
+     * @param locator - the element to click
+     */
+     async click(locator: By) {
+        await this.driver.wait(until.elementLocated(locator));
+        return (await this.driver.findElement(locator)).click();
+    }
+
+    /**
+     * Returns the text inside of a given element
+     * @param locator - the location of the element
+     * @returns string of text
+     */
+    async getText(locator: By) {
+        await this.driver.wait(until.elementLocated(locator));
+        return (await this.driver.findElement(locator)).getText();
+    }
 }
